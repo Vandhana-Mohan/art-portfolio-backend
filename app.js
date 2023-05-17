f304716cc2137350408aed20efa374782098a302
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-05-16 22:29:22 -0400

    set up appr.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+// const groceriesControllers = require("./controllers/groceriesControllers.js");
+// const reviewsController = require("./controllers/reviewsController.js");
+//CONFIG
+const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json()); //parses incoming json request
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome to Sanaaaa");
+});
+// app.use("/groceries", groceriesControllers);
+// app.use("/reviews", reviewsController);
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
+
+module.exports = app;
