664296747156911a119f685f9ba3438ea6dcab58
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-05-17 00:10:50 -0400

    set up schema

--- db/schema.sql
+++ db/schema.sql
@@ -10,14 +10,31 @@ CREATE TABLE userData (
     name VARCHAR(255) NOT NULL,
     about_artist TEXT,
     profession TEXT,
-    location VARCHAR(255),
-    theme VARCHAR(255),
-    collection_name TEXT,
+    location VARCHAR(255) NOT NULL,
+    instagram TEXT,
+    facebook TEXT,
+    tik_tok TEXT
+);
+
+CREATE TABLE artworks (
+    id SERIAL PRIMARY KEY,
+    collection_id INTEGER REFERENCES collections (id),
+    title VARCHAR(255) NOT NULL,
     price DECIMAL(10,2) NOT NULL CHECK (price >= 0),
     is_for_sale BOOLEAN DEFAULT false,
+    description TEXT,
+    image_url VARCHAR(255),
+    medium VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
+CREATE TABLE collections (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES userData (id),
+    title VARCHAR(255),
+    description TEXT,
+    theme VARCHAR(255),
+    collection_name TEXT
+);
 
