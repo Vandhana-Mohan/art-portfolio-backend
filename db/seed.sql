ec51fa785d49da155671713e708a828d15d85c5a
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-05-22 09:28:43 -0400

    add data

--- db/seed.sql
+++ db/seed.sql
@@ -1,16 +1,24 @@
 
 \c art_dev;
     
-INSERT INTO images (title, price, is_for_sale, description, image_url, medium) VALUES 
-    ('orange', 300.00, true, 'My first art', 'https://hips.hearstapps.com/hmg-prod/images/close-up-of-apple-against-white-background-royalty-free-image-1627315766.jpg', 'pencil'),
-    ('Modern Illustration', 200.00, true, 'My 2nd art', 'https://www.shutterstock.com/image-illustration/modern-illustration-linocut-style-surreal-260nw-1913052853.jpg', 'pen'),
-    ('Google', 0, false, 'My 3rd art', 'https://lh3.googleusercontent.com/ci/AJFM8rwqdr4P_2MEV_92ilEISXIY16I3ab9TKeyYy67BN86mP5Z5n9jWtys5zoHo5M9Xc935QBG7sQ', 'marker'),
-    ('Female Face', 0, false, 'My 4th art', 'https://media.gettyimages.com/id/1304289911/photo/my-vision-of-female-face.jpg?s=612x612&w=gi&k=20&c=0r_-G08H6S2EzJrQ33wzpWHhqqfX3FL-acefdYLzkbg=', 'sketch'),
-    ('Man with paint', 0, false, 'My 5th art', 'https://img.freepik.com/premium-photo/artist-man-with-paint-splash-her-head-creativity-3d-rendering_691560-3441.jpg', 'photo'),
-    ('Back to School', 0, false, 'My 6th art', 'https://st.depositphotos.com/1987395/1941/i/450/depositphotos_19414393-stock-photo-back-to-school-series-art.jpg', 'pencil'),
-    ('Dream Time', 0, false, 'My 7th art', 'https://thumbs.dreamstime.com/b/art-6260031.jpg', 'pencil'),
-    ('Woman Face', 0, false, 'My 8th art', 'https://img.freepik.com/premium-photo/woman-s-face-is-made-up-geometric-shapes-cyberpunk-colorful-fractalism-cubism_834088-1.jpg', 'pen'),
-    ('Xavier', 0, false, 'My 9th art', 'https://i0.wp.com/xaviernewswire.com/wp-content/uploads/2020/02/art.jpg?fit=948%2C572&ssl=1', 'sketch'),
-    ('Social Share', 0, false, 'My 10th art', 'https://static.wixstatic.com/media/c73ca7_c2c86fb932ad470a962988eedec67444~mv2.jpg/v1/fill/w_566,h_552,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/_edited.jpg', 'pencil'),
-    ('Magazines', 0, false, 'My 11th art', 'https://www.tccd.edu/magazine/assets/images/volume-03/issue-02/arts/the-importance-of-art-social-share.jpg', 'sketch'),
-    ('Used Brushes', 0, false, 'My 12th art', 'https://media.istockphoto.com/id/636761588/photo/used-brushes-on-an-artists-palette-of-colorful-oil-paint.jpg?s=612x612&w=0&k=20&c=38YQxVJVWnNfvGtlb7AXMx_ItyHZMEdmWenNkWNQ91g=', 'paint');
+INSERT INTO images (title, theme, price, is_for_sale, description, image_url, medium) VALUES 
+    ('Graphics', 'Art',300.00, true, 'My first art', 'https://cdn.mos.cms.futurecdn.net/6bxva8DmZvNj8kaVrQZZMP.jpg', 'pencil'),
+    ('Modern Illustration', 'Design', 200.00, true, 'My 2nd art', 'https://www.shutterstock.com/image-illustration/modern-illustration-linocut-style-surreal-260nw-1913052853.jpg', 'pen'),
+    ('Google', 'Creative Direction', 0, false, 'My 3rd art', 'https://lh3.googleusercontent.com/ci/AJFM8rwqdr4P_2MEV_92ilEISXIY16I3ab9TKeyYy67BN86mP5Z5n9jWtys5zoHo5M9Xc935QBG7sQ', 'marker'),
+    ('Female Face', 'Fashion', 0, false, 'My 4th art', 'https://media.gettyimages.com/id/1304289911/photo/my-vision-of-female-face.jpg?s=612x612&w=gi&k=20&c=0r_-G08H6S2EzJrQ33wzpWHhqqfX3FL-acefdYLzkbg=', 'sketch'),
+    ('Man with paint', 'Sports', 0, false, 'My 5th art', 'https://img.freepik.com/premium-photo/artist-man-with-paint-splash-her-head-creativity-3d-rendering_691560-3441.jpg', 'photo'),
+    ('Back to School', 'Design', 0, false, 'My 6th art', 'https://st.depositphotos.com/1987395/1941/i/450/depositphotos_19414393-stock-photo-back-to-school-series-art.jpg', 'pencil'),
+    ('Dream Time', 'Photography', 0, false, 'My 7th art', 'https://thumbs.dreamstime.com/b/art-6260031.jpg', 'pencil'),
+    ('Woman Face', 'All', 0, false, 'My 8th art', 'https://img.freepik.com/premium-photo/woman-s-face-is-made-up-geometric-shapes-cyberpunk-colorful-fractalism-cubism_834088-1.jpg', 'pen'),
+    ('Xavier', 'All', 0, false, 'My 9th art', 'https://i0.wp.com/xaviernewswire.com/wp-content/uploads/2020/02/art.jpg?fit=948%2C572&ssl=1', 'sketch'),
+    ('Social Share', 'Design', 0, false, 'My 10th art', 'https://static.wixstatic.com/media/c73ca7_c2c86fb932ad470a962988eedec67444~mv2.jpg/v1/fill/w_566,h_552,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/_edited.jpg', 'pencil'),
+    ('Magazines', 'Fashion', 0, false, 'My 11th art', 'https://www.tccd.edu/magazine/assets/images/volume-03/issue-02/arts/the-importance-of-art-social-share.jpg', 'sketch'),
+    ('Fashion', 'Fashion', 250, true, 'Lady with bag', 'https://media.istockphoto.com/id/541149082/photo/red-coat-woman-with-black-leather-handbag-beautiful-vintage-style.jpg?s=612x612&w=0&k=20&c=rFNgRblDUsr86srkukC0aItM4YoxRNKAoHOmUu9y45o=','photo'),
+    ('Women Wear', 'Fashion', 250, true, 'Trendy Shirts', 'https://thumbs.dreamstime.com/b/fashion-clothes-clothing-rack-colorful-closet-bright-closeup-rainbow-color-choice-trendy-female-wear-hangers-store-69750723.jpg','photo'),
+    ('Women Wear', 'Fashion', 250, true, 'Trendy Shirts', 'https://youtube.com/shorts/pzf6FcwMFsA?feature=share','photo'),
+    ('Women Wear', 'Fashion', 250, true, 'Trendy Shirts', 'https://youtube.com/shorts/3gxFb5JGylY?feature=share','photo'),
+    ('Women Wear', 'Fashion', 250, true, 'Trendy Shirts', 'https://i.pinimg.com/550x/68/76/c7/6876c787e4523dafdc34e30b3f9e3fb5.jpg','photo'),
+    ('Women Wear', 'Fashion', 250, true, 'Trendy Shirts', 'https://w0.peakpx.com/wallpaper/417/640/HD-wallpaper-cute-adorable-girl-baby-is-looking-up-wearing-purple-dress-cute.jpg','photo'),
+    ('Women Wear', 'Fashion', 250, true, 'Trendy Shirts', 'https://d.newsweek.com/en/full/1310267/best-hawaii-beaches.jpg','photo'),
+    ('Women Wear', 'Fashion', 250, true, 'Trendy Shirts', 'https://hips.hearstapps.com/hmg-prod/images/champagne-beach-espiritu-santo-island-vanuatu-royalty-free-image-1655672510.jpg','photo'),
+    ('Sports', 'Sports', 0, false, 'My 12th art', 'https://www.shutterstock.com/image-vector/color-sport-background-football-basketball-260nw-1315841180.jpg', 'paint');
